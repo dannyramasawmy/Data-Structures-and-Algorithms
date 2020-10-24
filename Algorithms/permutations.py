f102cf5a3d52a3eb4597e74e0b7ec1945fbedc3e
Danny Ramasawmy <rmapdrr@ucl.ac.uk>
2020-10-24 13:35:36 +0100

    First commit.

---
+++ Algorithms/permutations.py
@@ -0,0 +1,18 @@
+import math
+
+def strPermute(s, myset, l=''):
+    """
+    Print permutations.
+    """
+    if len(s) == 0:
+        print(l+'')
+        myset.add(l+s)
+    else:
+        for idx, ll in enumerate(s):
+            subs = s[0:idx] + s[idx+1:]
+            strPermute(subs, myset, l+ll)
+
+mystring = '1234'
+myset = set()
+strPermute(mystring, myset)
+
