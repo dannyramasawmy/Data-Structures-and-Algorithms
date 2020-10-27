c02c3c225819300d93e356f0296771ca1ec601a4
Danny Ramasawmy <rmapdrr@ucl.ac.uk>
2020-10-27 14:38:02 +0000

    Updates, tests.

--- DataStructures/LinkedList.py
+++ DataStructures/LinkedList.py
@@ -2,6 +2,7 @@
 #   CLASSES
 # ===========================================================
 
+
 class Node():
     """
     Node : a node on the linked list.
@@ -70,8 +71,21 @@ class LinkedList():
             counter += 1
         return None
 
+    def reverse(self):
+        """
+        reverse - reverses the linked list
+        """
+        # initialize
+        dragger, current, runner = None, None, self.head
+        while runner:
+            dragger = current
+            current = runner
+            runner = runner.next
+            current.next = dragger
+        self.head = current
+
     @staticmethod
-    def toLinkedList(items : list):
+    def toLinkedList(items: list):
         """
         toLinkedList - converts a python list to linked list
         """
@@ -120,65 +134,122 @@ def testNode():
     """
     Tests for the node object.
     """
+    try:
+        # simple check initializing a Node
+        n1 = Node(4)
+        assert n1.value == 4
+        assert n1.next == None
+        # new Node
+        n2 = Node(5)
+        assert n2.value == 5
+        assert n2.next == None
+        n1.next = n2
+        assert n1.next == n2
 
-    # simple check initializing a Node
-    n1 = Node(4)
-    assert n1.value == 4
-    assert n1.next == None
-    # new Node
-    n2 = Node(5)
-    assert n2.value == 5
-    assert n2.next == None
-    n1.next = n2
-    assert n1.next == n2
+        return True
+    except:
+        return False
 
 
-def testLinkedList():
-    """
-    Tests linked list.
-    """
-    # create a list add to the head
-    myList = LinkedList()
-    for i in range(4):
-        myList.addHead(i)
-        assert myList.length == i+1
-    assert f'{myList}' == " 3 -> 2 -> 1 -> 0 -> None"
-    # check with dunder method
-    assert myList.length == len(myList)
+def testAddHead():
+    try:
+        # create a list add to the head
+        myList = LinkedList()
+        for i in range(4):
+            myList.addHead(i)
+            assert myList.length == i+1
+        assert f'{myList}' == " 3 -> 2 -> 1 -> 0 -> None"
+        # check with dunder method
+        assert myList.length == len(myList)
 
-    # create a list add to the tail
-    myList = LinkedList()
-    for i in range(4):
-        myList.addTail(i)
-        assert myList.length == i+1
-    assert f'{myList}' == " 0 -> 1 -> 2 -> 3 -> None"
-    # check with dunder method
-    assert myList.length == len(myList)
+        return True
+    except:
+        return False
 
-    # check indexing
-    myList = LinkedList()
-    for i in range(4):
-        myList.addTail(i)
-        assert myList[i].value == i
 
-    # change a node in the linked list    
-    myList[2].value = 7
-    assert f'{myList}' == " 0 -> 1 -> 7 -> 3 -> None"
+def testAddTail():
+    try:
+        # create a list add to the tail
+        myList = LinkedList()
+        for i in range(4):
+            myList.addTail(i)
+            assert myList.length == i+1
+        assert f'{myList}' == " 0 -> 1 -> 2 -> 3 -> None"
+        # check with dunder method
+        assert myList.length == len(myList)
 
-    # check find
-    myList = LinkedList()
-    for i in 'alsjdonfkle':
-        myList.addTail(i)
-    assert f"{myList.find('e')}" == "(10, e ->)"
-    assert f"{myList.find('l')}" == "(1, l ->)"
-    assert f"{myList.find('z')}" == "None"
+        return True
+    except:
+        return False
 
-    # check linked list
-    myList = LinkedList.toLinkedList([0, 1, 2, 3])
-    assert f'{myList}' == " 0 -> 1 -> 2 -> 3 -> None"
-    
-    myList = LinkedList.toLinkedList([0, 'a', 7, 3])
-    assert f'{myList}' == " 0 -> a -> 7 -> 3 -> None"
+
+def testGetItem():
+    try:
+        # check indexing
+        myList = LinkedList()
+        for i in range(4):
+            myList.addTail(i)
+            assert myList[i].value == i
+
+        # change a node in the linked list
+        myList[2].value = 7
+        assert f'{myList}' == " 0 -> 1 -> 7 -> 3 -> None"
+
+        return True
+    except:
+        return False
+
+
+def testFind():
+    try:
+        # check find
+        myList = LinkedList()
+        for i in 'alsjdonfkle':
+            myList.addTail(i)
+        assert f"{myList.find('e')}" == "(10, e ->)"
+        assert f"{myList.find('l')}" == "(1, l ->)"
+        assert f"{myList.find('z')}" == "None"
+
+        return True
+    except:
+        return False
+
+
+def testListConvert():
+    try:
+        # check method for creating lists
+        myList = LinkedList.toLinkedList([0, 1, 2, 3])
+        assert f'{myList}' == " 0 -> 1 -> 2 -> 3 -> None"
+        myList = LinkedList.toLinkedList([0, 'a', 7, 3])
+        assert f'{myList}' == " 0 -> a -> 7 -> 3 -> None"
+
+        return True
+    except:
+        return False
+
+
+def testReverseList():
+    try:
+        # check reversing lists
+        myList = LinkedList.toLinkedList([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        myList2 = LinkedList.toLinkedList([0, 1, 2, 3, 4, 5, 6, 7, 8])
+        myRevList = LinkedList.toLinkedList([8, 7, 6, 5, 4, 3, 2, 1, 0])
+        myList.reverse()
+        assert f'{myList}' == f'{myRevList}'
+        myList.reverse()
+        assert f'{myList}' == f'{myList2}'
+
+        return True
+    except:
+        return False
+
+
+def test(func, name):
+    if func:
+        print(f'Test {name} : Pass')
+    else:
+        print(f'Test {name} : Fail')
+
 
 # ===========================================================
 #   MAIN PROGRAM
@@ -186,14 +257,11 @@ def testLinkedList():
 if __name__ == "__main__":
     # test node
     print(10*'--')
-    print('Test Node')
-    testNode()
-    print('Tests passed')
-    print(10*'--')
-
-    # test linked list
-    print(10*'--')
-    print('Test LinkedList')
-    testLinkedList()
-    print('Tests passed')
+    test(testNode(), 'node class')
+    test(testAddHead(), 'add at head')
+    test(testAddTail(), 'add at tail')
+    test(testGetItem(), 'get the item')
+    test(testFind(), 'find a node')
+    test(testListConvert(), 'convert list to linked list')
+    test(testReverseList(), 'reverse list')
     print(10*'--')
