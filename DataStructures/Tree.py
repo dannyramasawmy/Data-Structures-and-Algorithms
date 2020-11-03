eab510cbb5cb81421478fd38147c28fc3a5d2455
Danny Ramasawmy <rmapdrr@ucl.ac.uk>
2020-11-03 16:05:36 +0000

    Small edit.

--- DataStructures/Tree.py
+++ DataStructures/Tree.py
@@ -15,3 +15,47 @@ class BinarySearchTree():
     Binary search tree.
     """
     pass
+
+
+
+
+class Heap():
+    """
+    Heap min
+    """
+    def __init__(self):
+        """
+        Initialise heap
+        """
+        self.array = []
+
+    def getParentIndex(self, index):
+        """
+        get the parent index of the child node
+        """
+        pass
+
+    def insert(self, value):
+        """
+        insert - add a node to the bottom, bubble the node up the tree
+        """
+        if self.head == None:
+            self.head = Node(value)
+
+    def extract(self):
+        """
+        extract - take the top element, bubble the node down the tree
+        """
+        pass
+
+    def peek(self):
+        """
+        peek - see the min/max of the tree
+        """
+        return 
+
+    def heapify(self):
+        """
+        heapify - reorders tree
+        """
+        pass
