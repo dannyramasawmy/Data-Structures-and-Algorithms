2e1c040376a4869dd80cc7c5a6694eef7791da58
Danny Ramasawmy <rmapdrr@ucl.ac.uk>
2020-12-02 14:38:04 +0000

    Secret santa.

---
+++ Misc/secretSanta.py
@@ -0,0 +1,19 @@
+from random import shuffle
+
+def createGraph(names : list) -> dict:
+    tmp = names
+    shuffle(tmp)
+
+    identifier = dict()
+    for idx in range(len(tmp)):
+        identifier[tmp[idx]] = tmp[(idx + 1)%len(tmp)]  
+
+    return identifier
+
+def printGraph(identifier : dict) -> None:
+    for name in identifier.keys():
+        print(f'{name} -> {identifier[name]}')
+
+names = ['A', 'B', 'D', 'E', 'Ma', 'Mi', 'Mo', 'Sa']
+
+printGraph(createGraph(names))
